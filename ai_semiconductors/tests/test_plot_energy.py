b0bb4e603f873b3bcab2e937a65777dfb140b870
Xiao <xiaofx2@uw.edu>
2020-06-23 21:45:22 -0700

    test

---
+++ ai_semiconductors/tests/test_plot_energy.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import sys
+sys.path.append("../")
+
+
+# In[3]:
+
+
+def test_plot_energy():
+    import plot_energy
+    import os
+    sys.path.append(os.path.abspath('../'))
+    os.chdir('../')
+    plot_energy.plot_energy('Cu', 'CdSe', 'M_A')
+    assert 1 + 1 == 2, "Plotting doesn't work"
+
+
+# In[ ]:
+
+
+
+
